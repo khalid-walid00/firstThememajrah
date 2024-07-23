1b0a5422e98ee07e6db31dd7d4de0b64e79ee6af
khalidwalid00 <khalidk50050@gmail.com>
2024-07-23 12:38:33 +0300

    updateslide

--- src/controllers/product.controller/index.js
+++ src/controllers/product.controller/index.js
@@ -3,6 +3,6 @@ const client = require("../../utils/qumraClient");
 module.exports = async (req, res, next) => {
     const {slug} = req.params
     const product = (await client.getProduct({ slug })).data;
-    console.log("🚀 ~ module.exports= ~ product:", product)
-    res.render("product", {product})
+    const products = (await client.getProducts({ limit: 10, page: 1 }))?.data;
+    res.render("product", {product,products})
   }
