99e31d2c166d55b2df31b0358827111b96c36aea
khalidwalid00 <khalidk50050@gmail.com>
2024-07-23 06:50:49 +0300

    update

--- src/controllers/products.controller/index.js
+++ src/controllers/products.controller/index.js
@@ -1,7 +1,8 @@
 const client = require("../../utils/qumraClient");
 
 module.exports = async (req, res, next) => {
-    const {slug} = req.params
-    const product = (await client.getProduct({ slug })).data;
+    // const {slug} = req.params
+    const product = (await client.getProduct()).data;
+    console.log("🚀 ~ module.exports= ~ products:", product)
     res.render("products.njk")
   }
